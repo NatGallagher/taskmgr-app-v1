2b34785af52a2fcfc0d4294b8fe0c47c1d4137cd
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-27 15:02:41 -0400

    version updated

--- taskmgr-app-v1/src/App.js
+++ taskmgr-app-v1/src/App.js
@@ -1,5 +1,5 @@
-import {HashRouter, Routes, Route} from "react-router-dom"
-import Home from "./components/Home"
+import {HashRouter, Routes, Route} from "react-router-dom";
+import Home from "./components/Home";
 import ContactUs from "./components/ContactUs";
 
 function App() {
