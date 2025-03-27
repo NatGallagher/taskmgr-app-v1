2059f140b3d571762892abef655d4d4223d3d935
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-27 18:15:07 -0400

    contact info added

--- taskmgr-app-v1/src/components/ContactUs.js
+++ taskmgr-app-v1/src/components/ContactUs.js
@@ -2,6 +2,17 @@ function ContactUs() {
     return (
       <>
         <h2>Contact Us</h2>
+        <form>
+            <div>
+                <span>First Name: </span><input type="text" maxlength={25} placeholder="* First Name"/>
+                <br/>
+                <span>Last Name: </span><input type="text" maxlength={25} placeholder="Last Name"/>
+                <br/>
+                <span>Email: </span><input type="text" maxlength={100} placeholder="* Email"/>                
+                <p></p>
+                <button>Submit</button> {" "} <a href="#">clear</a>
+            </div>
+        </form>
       </>
     );
   }
