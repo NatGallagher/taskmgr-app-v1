943ce691f712abec070b4e2c0436b54cfcbd0785
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-27 15:27:26 -0400

    NavBar created

--- taskmgr-app-v1/src/App.js
+++ taskmgr-app-v1/src/App.js
@@ -2,11 +2,13 @@ import {HashRouter, Routes, Route} from "react-router-dom";
 import Home from "./components/Home";
 import ContactUs from "./components/ContactUs";
 import About from "./components/About";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <HashRouter>        
+      <HashRouter>
+        <NavBar/>        
       <Routes>      
         <Route path="/" element={<Home/>}/>
         <Route path="/contact" element={<ContactUs/>}/>
