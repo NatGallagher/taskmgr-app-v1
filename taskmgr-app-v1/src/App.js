d5a922ae13064018318849b7ec78b914f192d8c4
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-27 15:19:43 -0400

    About.js created and imported

--- taskmgr-app-v1/src/App.js
+++ taskmgr-app-v1/src/App.js
@@ -1,6 +1,7 @@
 import {HashRouter, Routes, Route} from "react-router-dom";
 import Home from "./components/Home";
 import ContactUs from "./components/ContactUs";
+import About from "./components/About";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>      
         <Route path="/" element={<Home/>}/>
         <Route path="/contact" element={<ContactUs/>}/>
+        <Route path="/about" element={<About/>}/>
       </Routes>
     </HashRouter>    
     </>
