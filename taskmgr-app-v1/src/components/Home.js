f3f1f4df110386fa02da3b9d9110a8b3dfec6f80
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-28 12:15:18 -0400

    contact and home edited

--- taskmgr-app-v1/src/components/Home.js
+++ taskmgr-app-v1/src/components/Home.js
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom"
 function Home() {
     return (
       <>
-        <h2>Task Manager App v1.4</h2>
+        <h2>Task Manager App v1.5</h2>
         <p></p>
         <div>
             <input type="text" maxLength={25} placeholder="* add item"/>
@@ -20,6 +20,12 @@ function Home() {
           <span>[x] itemb -x-</span><br/>
           <span>[] itemc -x-</span><br/>
           <span>[x] itemd -x-</span>
+        </div>
+        <div>
+          <p>Version involves:</p>
+          [Items/
+          all,complete,incomplete/
+          text box in contact us]
         </div>
       </>
     );
